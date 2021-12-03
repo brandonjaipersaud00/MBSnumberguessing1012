85aef371307906c947e685061afb3ecc8a4c02c6
mahnsi <90293053+mahnsi@users.noreply.github.com>
2021-12-03 12:59:37 -0500

    Update guessinggame.js

--- guessinggame.js
+++ guessinggame.js
@@ -1,9 +1,11 @@
-function Play(guesses) {
-  
-    var x = guesses;
-  
+var x = document.getElementById("drop").value;
+
+function Play() {
+    var outputObj=document.getElementById("output");
+    outputObj.innerHTML= x;
   }
-  
+
+
   
   function Leader() {
   
