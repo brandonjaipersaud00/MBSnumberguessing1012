7e52d5461b93c9769892855f494f8669080b86ae
brandonjaipersaud00 <92167279+brandonjaipersaud00@users.noreply.github.com>
2021-12-03 17:09:34 -0500

    updated

--- guessinggame.js
+++ guessinggame.js
@@ -1,8 +1,22 @@
 var x = document.getElementById("drop").value;
+var n = Math.floor(Math.random() * 100) + 1;
+var result = document.getElementById("result");
+var submit = document.getElementById("submit");
+var entry = document.getElementById("entry");
+var cnt = 1;
 
 function Play() {
-    var outputObj=document.getElementById("output");
-    outputObj.innerHTML= x;
+    var Uservalue = Number(entry.value);
+    if(Uservalue === n) {
+      result.innerHTML = 'That is correct!';
+      Winner();
+    } else if(Uservalue === 25) {
+      result.innerHTML = 'GAME OVER';
+      GameOver();
+    } else {
+      result.innerHTML = 'Wrong!';
+    }
+    cnt++;
   }
 
 
