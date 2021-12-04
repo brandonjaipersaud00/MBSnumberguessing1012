e9ff9bacb614b11e4f5dc1861f4402c5bdf73063
mahnsi <90293053+mahnsi@users.noreply.github.com>
2021-12-04 15:26:53 -0500

    Update guessinggameLeaderboard.js

--- guessinggameLeaderboard.js
+++ guessinggameLeaderboard.js
@@ -1,8 +1,14 @@
-var fname   = document.getElementById("fname").value;
-var lname    = document.getElementById("lname").value;
-var numentry = document.getElementById("numentry").value;
+var fname   = document.getElementsByTagName("fname").value = window.location.search;
+
+var lname    = document.getElementsByTagName("lname").value= window.location.search;
+
+var numentry = document.getElementsByTagName("numentry").value= window.location.search;
+numentry = numentry.substring(numentry.length-2);
+numentry = parseInt(numentry);
+
 var leaderboard = [ fname, lname, numentry ];
 
 leaderboard.sort(function(a,b){
     return parseFloat(a.numentry) - parseFloat(b.numentry);
 })
+
