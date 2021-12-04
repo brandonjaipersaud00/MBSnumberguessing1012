a29144db17710d02876e7828b0f5b2228c7ce934
mahnsi <90293053+mahnsi@users.noreply.github.com>
2021-12-04 02:36:27 -0500

    Update guessinggame.js

--- guessinggame.js
+++ guessinggame.js
@@ -1,18 +1,12 @@
-
-
 var n = Math.floor(Math.random() * 100) + 1; //chosen random number
 var cnt = 1; //counter up to number of tries based on chosen game mode
 var entry_tries = 1 //number of tries to get chosen number
 
-//gets the value of the level button (easy, med, hard, insane), and parses it to an integer
+//gets the value of the level button (easy, med, hard, insane), extracts just the number part, and parses it to an integer
 var guessnumb = document.getElementsByTagName("level").innerHTML = window.location.search;
 guessnumb.toString();
 guessnumb = guessnumb.substring(7);
 guessnumb = parseInt(guessnumb);
-
-function startGuess(){
-  document.getElementById("starting").innerHTML = "you have " + guessnumb +" guesses";
-}
 
 
 function Play() {
@@ -21,16 +15,19 @@ function Play() {
   if(entry === n) {
     document.getElementById("output").innerHTML = 'That is correct! It took you ' + entry_tries + ' time(s) to guess it.';
     Winner();
-  } else if(entry === guessnumb) {
-    //!!ENTRY SHOULD BE EQUAL TO AMOUNT OF TRIES BASED ON CHOSEN GAME MODE!!
-    document.getElementById("output").innerHTML = 'GAME OVER';
-    GameOver();
-  } else {
+  }  else {
     document.getElementById("output").innerHTML = 'Wrong!';
     entry_tries++;
   }
+
+  if(cnt === guessnumb) {
+    //!!ENTRY SHOULD BE EQUAL TO AMOUNT OF TRIES BASED ON CHOSEN GAME MODE!!
+    document.getElementById("output").innerHTML = 'GAME OVER';
+    GameOver();
+  }
   cnt++;
 }
+  
 
 
 //for disabling and enabling buttons
@@ -51,9 +48,4 @@ function GameOver() {
   
   function Leader() {
   
-  }
-  
-  function Weather (){
-  
-    
   }
