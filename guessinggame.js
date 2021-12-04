2f54a900ab943ef48652a7aa866d0fe57569ba13
mahnsi <90293053+mahnsi@users.noreply.github.com>
2021-12-04 01:27:09 -0500

    figured out the number of guesses thing

--- guessinggame.js
+++ guessinggame.js
@@ -1,41 +1,59 @@
-<!DOCTYPE html>
-<html lang="En">
-<head>
-  <meta charset="UTF-8">
-  <title> Guess the Number </title>
-  <link rel="stylesheet" type="text/css" href="guessinggame.css" title="style">
-  <script src="guessinggame.js"></script>
-</head>
-<body>
 
 
-<header>
-  <!--- weather widget (can be placeholder for now) ---> 
-    <h1><script src="https://apps.elfsight.com/p/platform.js" defer></script>
-  <div class="elfsight-app-c174dcb7-0545-4ec3-9713-ff143067c30d"></div></h1>
+var n = Math.floor(Math.random() * 100) + 1; //chosen random number
+var cnt = 1; //counter up to number of tries based on chosen game mode
+var entry_tries = 1 //number of tries to get chosen number
+
+//gets the value of the level button (easy, med, hard, insane), and parses it to an integer
+var guessnumb = document.getElementsByTagName("level").innerHTML = window.location.search;
+guessnumb.toString();
+guessnumb = guessnumb.substring(7);
+guessnumb = parseInt(guessnumb);
+
+function startGuess(){
+  document.getElementById("starting").innerHTML = "you have " + guessnumb +" guesses";
+}
+
+
+function Play() {
   
-  <h2>Guess the Number</h2>
-</header>
-
-<!--- dropdown menu for easy medium hard and insane---> 
-<!--- each button submits a different value based on the level (number of guesses)---> 
-<nav>
-    <div class="dropdown">
-    <button class="dropbtn">Play</button>
-    <div class="dropdown-content">
-      <form action="guessinggamePlay.html" method="get">
-    <button id = drop name="level" type="submit" value="25">Easy</button>
-    <button id = drop name="level" type="submit" value="15">Medium</button>
-    <button id = drop name="level" type="submit" value="5">Hard</button>
-    <button id = drop name="level" type="submit" value="1">Insane</button>
-    </form>
-    </div>
-  </div>
-<!--- buttons for how to play and leaderboard. they each redirect to a new html page.---> 
-    <button onclick="window.location.href='guessinggameHowTo.html'">How To Play</button>
-    <button onclick="window.location.href='guessinggameLeaderboard.html'">Leaderboard</button> 
-</nav>
+  var entry = parseInt(document.getElementById("entry").value);
+  if(entry === n) {
+    document.getElementById("output").innerHTML = 'That is correct! It took you ' + entry_tries + ' time(s) to guess it.';
+    Winner();
+  } else if(entry === guessnumb) {
+    //!!ENTRY SHOULD BE EQUAL TO AMOUNT OF TRIES BASED ON CHOSEN GAME MODE!!
+    document.getElementById("output").innerHTML = 'GAME OVER';
+    GameOver();
+  } else {
+    document.getElementById("output").innerHTML = 'Wrong!';
+    entry_tries++;
+  }
+  cnt++;
+}
 
 
-</body>
+//for disabling and enabling buttons
+function Winner() {
+  document.getElementById("submit").disabled = true;
+  document.getElementbyId("Home").disabled = true;
+  document.getElementbyId("Leaderboard Entry").disabled = false;
+}
 
+
+function GameOver() {
+    document.getElementById("submit").disabled = true;
+    document.getElementbyId("Home").disabled = false;
+    document.getElementbyId("Leaderboard Entry").disabled = true;
+      }
+
+
+  
+  function Leader() {
+  
+  }
+  
+  function Weather (){
+  
+    
+  }
