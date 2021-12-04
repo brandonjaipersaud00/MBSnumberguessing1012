448ed08e30ae9d6fffe70aafb33bd8b4c285c515
mahnsi <90293053+mahnsi@users.noreply.github.com>
2021-12-04 16:29:29 -0500

    Update guessinggameLeaderboard.js

--- guessinggameLeaderboard.js
+++ guessinggameLeaderboard.js
@@ -1,14 +1,23 @@
+//gets name that was entered
 var fname   = document.getElementsByTagName("fname").value = window.location.search;
+fname = fname.replace("?fname=","");
+fname = fname.replace("&numentry=","");
+fname=fname.substring(0, fname.length - 2);
 
-var lname    = document.getElementsByTagName("lname").value= window.location.search;
-
+//gets number of entries that was entered
 var numentry = document.getElementsByTagName("numentry").value= window.location.search;
 numentry = numentry.substring(numentry.length-2);
 numentry = parseInt(numentry);
 
-var leaderboard = [ fname, lname, numentry ];
+//array 
+var leaderboard = [ fname, numentry ];
+
+//when a level is clicked, is prints the leaderboard with the name that was entered  and score
+function printLeaderBoard(){
+    document.getElementById("name1").innerHTML = fname;
+    document.getElementById("score1").innerHTML = numentry;
+}
 
 leaderboard.sort(function(a,b){
     return parseFloat(a.numentry) - parseFloat(b.numentry);
 })
-
