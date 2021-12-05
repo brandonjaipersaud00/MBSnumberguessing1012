34a80d0886a3fc4aad0577feb33ad10608ea9067
mahnsi <90293053+mahnsi@users.noreply.github.com>
2021-12-04 21:16:43 -0500

    Update guessinggameserver.js

--- guessinggameserver.js
+++ guessinggameserver.js
@@ -1,3 +1,9 @@
 var N = Math.floor(Math.random() * 100) + 1;
 var CNT = 1; //counter up to number of tries based on chosen game mode
 var ENTRY_TRIES = 1 //number of tries to get chosen number
+
+var codes ={};
+
+var express = require('express');
+var app = express();
+var idCounter = 0;
