004bc50536040539f23e96e7cab253fc6d7020a4
brandonjaipersaud00 <92167279+brandonjaipersaud00@users.noreply.github.com>
2021-12-04 22:59:55 -0500

    update

--- guessinggameserver.js
+++ guessinggameserver.js
@@ -7,3 +7,33 @@ var codes ={};
 var express = require('express');
 var app = express();
 var idCounter = 0;
+
+if ('action' == 'entry'){
+    idCounter++
+    var jsontext = JSON.stringify({
+        win = true,
+        'entry_tries': entry_tries,
+        'guessnumb': guessnumb,
+        'code' : answer
+    });
+}
+else{
+    var [entry_tries, guessnumb]
+    = evaluate (codes[guessnumb] [answer]);
+}
+
+var win=false;
+if (entry_tries < guessnumb) win = true;
+
+var jsontext = JSON.stringify({
+    win = true,
+    'entry_tries': entry_tries,
+    'guessnumb': guessnumb,
+    'code' : answer
+});
+res.send(jsontext);
+
+function evaluate(guessnumb, answer) {
+    var entry_tries = 0;
+    var guessnumb = 0;
+}
